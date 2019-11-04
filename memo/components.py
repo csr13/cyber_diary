3192207b61889431302fbeed9a9fd396566fb900
pxyg13 <carsaroca.13@gmail.com>
2019-11-03 20:10:51 -0600

    cleaned up

--- memo/components.py
+++ memo/components.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# csr
 """
 Copyright 2019 C. S. R.
 
@@ -14,6 +15,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
 import csv
 
 from colors import c
@@ -21,138 +23,70 @@ from colors import c
 
 class Menu:
 
-    """
-
-    Menu class.
-
-    Params
-    ------
-
-    main_choices       :: list of int choices for the instanciated menu
-                           
-    -----------------------------------------------------------------------
-
-    Functions
-    ---------
-
-    menu_choice :: gets a menu choice from the user; if given choice is the-
-    *main* self.choices, it procceds to set it up as the current choice.
-
-    """
-
     def __init__(self, main_choices=None):
-
         if main_choices and len(main_choices) >= 1:
-
-                self.main_choices = main_choices
-
+            self.main_choices = main_choices
         else:
-
             self.main_choices = None
 
 
     def menu_choice(self):
-        ''' Gets a menu choice for the given menu main_choices '''
-
-        ticker  = c("<(:)>", "pink")
-
+        """
+        Gets a menu choice for the given menu main_choices
+        """
         pointer = c(">", "pink")
-
-        choice = input("{} Option number {} ".format(ticker, pointer))
-
+        ticker  = c("<(:)>", "pink")
+        text    = c("Option number", "under_white")
+        choice = input(f"{ticker} {text} {pointer} ")
         try:
-
             if choice and choice in self.main_choices:
-
                 self.current_choice = choice
-
             else:
-
                 raise
-
         except Exception:
-
+            choices = ", ".join(self.main_choices)
             error_ticker = c("<(!)>", "lightr")
-            print(
-                '\n{} Choices available {} \n'.format(
-                                                error_ticker,
-                                                ', '.join(self.main_choices)
-                                                )
-                                            )
-            # recursion
+            error_message = f"\n{error_ticker} Choices available {choices}" 
+            print(error_message)
             self.menu_choice()
 
 
-
 class Entry:
-
-    """
-
-    Entry Object to save and retreive from a storage.
-
-    Functions:
-
-    save                :: check the docstring for info.
-    retreive            :: check the docstring for info.
-
-    """
-
+ 
     @classmethod
     def save(cls, **kwargs):
         """
         Save the entry to a given file via kwargs.
         """
-
         if kwargs:
-
             field_names = []
-
         if "file" in kwargs and len(kwargs) > 2:
-
             file = kwargs.get('file')
-            
             kwargs.__delitem__('file')
 
         for key in kwargs.keys():
-            
             field_names.append(key)
 
         with open(file, 'a', newline='') as db:
-            
             pincel = csv.DictWriter(db, fieldnames=field_names)
-            
             pincel.writerow(kwargs)
-            
             return True, '<Entry.create.save >> {}>'.format(file)
 
 
     @classmethod
     def retreive(cls, file=None, type='r'):
-
         """
         Retreives a reader from given file.
         """
-
-        if file and file[-4:] == '.csv':
-            
+        if file and file[-4:] == '.csv': 
             pass
-
         else:
-            
             raise Exception('Only csv files.') from None
-
         try:
-            
             with open(file, type, newline='') as file:
-            
                 lector = csv.reader(file, delimiter=',')
-            
                 return [entry for entry in lector]
-
         except FileNotFoundError:
-            
-            raise FileNotFoundError(
-                'File: %s not found in cwd' % (file)
-            )
+            raise FileNotFoundError(f"File: {file} not found in cwd") from None
 
 

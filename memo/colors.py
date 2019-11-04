ebbd8e0dd0c269ca384da0b2dd63178d66d5c71a
pxyg13 <carsaroca.13@gmail.com>
2019-11-03 20:10:36 -0600

    more colors

--- memo/colors.py
+++ memo/colors.py
@@ -1,55 +1,66 @@
 # -*- coding: utf-8 -*-
-# c.s.r :)
+# csr
 
-import random
+import os
+from collections import OrderedDict
 
-class Rainbow:
 
-    def __init__(self):
-        
-        self.black   = '\033[30m'
+colors = OrderedDict([
+    ("black"  , '\033[30m'),
+    ("red"    , '\033[31m'),
+    ("green"  , '\033[32m'),
+    ("orange" , '\033[33m'),
+    ("blue"   , '\033[34m'),
+    ("purple" , '\033[35m'),
+    ("lightr" , '\033[91m'),
+    ("lgreen" , '\033[92m'),
+    ("yellow" , '\033[93m'),
+    ("pink"   , '\033[95m'),
+    ("reset"  , "\033[0m"),
+])
 
-        self.red     = '\033[31m'
+underline_colors = OrderedDict([
+    ("under_black"  , "\033[4;30m"),
+    ("under_red"    , "\033[4;31m"),
+    ("under_green"  , "\033[4;32m"),
+    ("under_yellow" , "\033[4;33m"),
+    ("under_blue"   , "\033[4;34m"),
+    ("under_purple" , "\033[4;35m"),
+    ("under_cyan"   , "\033[4;36m"),
+    ("under_white"  , "\033[4;37m"),
+])
 
-        self.green   = '\033[32m'
+colors.update(underline_colors)
 
-        self.orange  = '\033[33m'
+class Palette():
 
-        self.blue    = '\033[34m'
+    def __init__(
+        self,
+        colors=colors
+    ):
+        for key, value in colors.items():
+            setattr(self, key, value)
 
-        self.purple  = '\033[35m'
 
-        self.lightr  = '\033[91m'
-
-        self.lgreen  = '\033[92m'
-
-        self.yellow  = '\033[93m'
-
-        self.pink    = '\033[95m'
-        
-        self.reset   = "\033[0m"
-
+class Pincel(Palette):
 
     @classmethod
-    def pincelada(cls, *args):
-        
+    def pincelada(this, *args):
         text, color = args
-        
-        this_class = Rainbow()
-
-        reset = this_class.reset
-        
-        if color in this_class.__dict__.keys():
-
-            color = this_class.__dict__[color]
-
-            return "%s%s%s" % (color, text, reset)
-        
+        _c = Pincel()
+        reset = _c.reset
+        if color in _c.__dict__.keys():
+            color = _c.__dict__[color]
+            return f"{color}{text}{reset}"
         return text
 
-# The main brush
 
-c = Rainbow.pincelada
+c = Pincel.pincelada
+
+flecha = c("> ", "purple")
+robot  = c("[", "pink")
+robot += c("*", "blue") + c("_", "red") + c("*", "blue")
+robot += c("]", "pink")
 
 
 

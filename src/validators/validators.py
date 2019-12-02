57de20eb2959426506f632a5dd1506802ba6ee02
pxyg13 <carsaroca.13@gmail.com>
2019-12-01 18:21:48 -0600

    this module is functional & tested

--- src/validators/validators.py
+++ src/validators/validators.py
@@ -1,23 +1,32 @@
+from datetime import datetime
 import sys
 import re
-sys.argv("..")
+sys.path.append("..")
 
 from creative.colors import c, flecha, robot
-
 
 
 DATE_RE = r"(\d{2})/(\d{2})/(\d{4})"
 DATE_RANGE_RE = r"(\d{2})/(\d{2})/(\d{4})-(\d{2})/(\d{2})/(\d{4})"
 
-def validate_date(date):
-    if not re.match(DATE_RE, date):
-        error = c("[!]", "red") + c("invalid date format", "under_white") 
-        raise AttributeError(error) from None
-    to_return  = date_group.split("/")
-    month = date_group(1).__int__()
-    day   = date_group(2).__int_()
-    year  = date_group(3).__int__()
-    date = datetime(year=year, month=month, day=day) 
-    return '/'.join()
 
-def validate_date_range(date_range):
+def validate_date(date):
+    # the date does not match the regular expression.
+    if not re.match(DATE_RE, date):
+        # make an error
+        error = c("[!]", "red") + c("invalid date format", "under_white") 
+        # raise An attributeError with the message.
+        raise AttributeError(error) from None
+    # split the date by dash and make a list of integers, to verify
+    # datetime wont take strings as parameters.
+    month, day, year = list(map(int, date.split("/")))
+    # try this.
+    try:
+        # verify that the date is actually a date.
+        datetime(year=year, month=month, day=day)
+        return date
+    # the datetime throws Value errors with info of where the code went wrong,
+    except ValueError as o_o:
+        # raise it as o_o
+        raise(o_o)
+        
